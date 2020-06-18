778c6542760c02c1e6fa24d17a00dfe0bdd22bb0
maximkiesel <maxim_kiesel@gmx.de>
2020-06-18 15:10:34 +0200

    Beleg 2 Mapanzeige und Speicherung fehlt

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 
 function hideLogin() {
-    var x = document.getElementById("LoginContainer");
-    x.style.display = "none"; 
-  }
-function showLogin(){
   var x = document.getElementById("LoginContainer");
-  x.style.display = "block";   
+  x.style.display = "none";
+}
+function showLogin() {
+  var x = document.getElementById("LoginContainer");
+  x.style.display = "block";
 }
 
 
@@ -24,24 +24,29 @@ function hideAdd() {
   x.style.display = "none";
 }
 function showAdd() {
-  document.getElementById("addTitle").innerHTML = "Add"
   var x = document.getElementById("AddContainer");
   x.style.display = "block";
+}
+
+function hideEdit() {
+  var x = document.getElementById("EditContainer");
+  x.style.display = "none";
 }
 function showEdit() {
-  document.getElementById("addTitle").innerHTML = "Edit"
-  var x = document.getElementById("AddContainer");
+  var x = document.getElementById("EditContainer");
   x.style.display = "block";
 }
 
 
-function hideAll(){
+
+
+function hideAll() {
   hideLogin();
   hideMain();
   hideAdd();
 }
 
-function showAll(){
+function showAll() {
   showLogin();
   showMain();
   showAdd();
@@ -61,16 +66,107 @@ function add() {
   showAdd();
 }
 
+function edit(index) {
+  showEdit();
+}
+
 function edit() {
   showEdit();
 }
 
-function backToLogin(){
+function backToLogin() {
   hideAll();
   showLogin();
 }
 
-function backToMain(){
+function backToMain() {
   hideAll();
   showMain();
 }
+
+class Contact {
+
+  vorname;
+  nachname;
+  straße;
+  plz;
+  stadt;
+  land;
+  isPrivate;
+
+  constructor(vorname, nachname, straße, plz, stadt, land, isPrivate) {
+    this.vorname = vorname;
+    this.nachname = nachname;
+    this.straße = straße;
+    this.plz = plz;
+    this.stadt = stadt;
+    this.land = land;
+    this.isPrivate = isPrivate;
+  }
+
+}
+
+contact1 = new Contact("Thomas", "Nofz", "Berlinerstraße 30", "13403", "Berlin", "Deutschland", false);
+contact2 = new Contact("Sven", "Schneider", "Berlinerstraße 30", "13403", "Berlin", "Deutschland", true);
+var contacts = [contact1, contact2];
+
+function addContact(vorname, nachname, straße, plz, stadt, land, isPrivate) {
+  contact = new Contact(vorname, nachname, straße, plz, stadt, land, isPrivate);
+  contacts.push(contact);
+  //alert(contact.vorname+" "+contact.nachname +" hinzugefügt")
+}
+
+function editContact(index, vorname, nachname, straße, plz, stadt, land, isPrivate) {
+  contacts[index] = new Contact(vorname, nachname, straße, plz, stadt, land, isPrivate);
+}
+
+function removeContact(index) {
+  contacts.splice(index, 1);
+}
+
+function printContacts(isAdmin) {
+  var x = "<tr><th>Name</th><th>Delete</th><th>Edit</th><th>Show</th></tr>";
+  for (i = 0; i < contacts.length; i++) {
+    if (isAdmin == true || contacts[i].isPrivate == false) {
+      x = x + "<tr><td>" + contacts[i].vorname + " " + contacts[i].nachname + "</td><td> <button type=\"button\" onclick=\"removeContactFunction(" + i + ");\">Delete Contact " + i + "</button></td><td> <button type=\"button\" onclick=\"editFunction("+i+")\">Edit</button></td><td> <input type=\"checkbox\"> </td></tr>";
+    }
+  }
+  return x;
+}
+
+
+class User {
+
+  username;
+  password;
+  isAdmin;
+
+  constructor(username, password, isAdmin) {
+    this.username = username;
+    this.password = password;
+    this.isAdmin = isAdmin;
+  }
+}
+admin = new User("admin", "1", true);
+normalo = new User("normalo", "2", false);
+
+var users = [admin, normalo];
+
+function loginAsUser(username, password) {
+
+  for (i = 0; i < users.length; i++) {
+    if (users[i].username == username && users[i].password == password) {
+      login();
+      return users[i];
+    }
+  }
+  alert("falscher Username oder Password");
+
+}
+
+function registerNewUser(username, password) {
+  user = new User(username, password, false);
+  users.push(user);
+  alert("neuer User mit dem Usernamen " + user.username + " registriert");
+}
+
